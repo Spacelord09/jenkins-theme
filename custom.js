e4f4e2187b1fdd269b6c03ddfa7f7300f0cbaa29
Spacelord <Spacelord09@users.noreply.github.com>
2022-02-12 22:35:20 +0100

    Initial commit

---
+++ custom.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById('jenkins-head-icon').src='https://static.spacecdn.de/logo/logo_eng.svg';
+});
+
---
+++ login.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('#loginIntroDefault > h1:nth-child(2)').innerHTML='Welcome to SpaceCI!';
+});
+
+
+
