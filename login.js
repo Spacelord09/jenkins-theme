8924970e081d192655d80d861c759f7124a6f2ae
Spacelord <Spacelord09@users.noreply.github.com>
2022-07-01 23:04:48 +0200

    Fix logo and general cleanup

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 	document.querySelector('#loginIntroDefault > h1:nth-child(2)').innerHTML='Welcome to SpaceCI!';
+	document.querySelector('.simple-page .logo > img').src = 'https://static.spacecdn.de/logo/logo_eng.svg'
 });
 
 
